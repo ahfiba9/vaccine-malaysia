44546a8775e2980a5ce24523a5a07be810c40be0
Ahmad Fikri <ahmad.fikri@aymcommerce.com>
2021-08-11 23:58:10 +0800

    insert data verification against empty entry at the end of excel file

--- components/LatestData.jsx
+++ components/LatestData.jsx
@@ -4,15 +4,29 @@ import {globalState} from "../library/globalState";
 export const LatestData = () => {
     const snap = useSnapshot(globalState)
 
-    const casesNumber = parseInt(snap.nationalCases[snap.nationalCases.length - 1]['Daily Cases'])
-    const casesNumberPrev = parseInt(snap.nationalCases[snap.nationalCases.length - 2]['Daily Cases'])
+    const data = snap.nationalCases
+
+    // check for empty entry at the end of file
+    let i = data.length
+    let j = 1
+
+    while (i !== 0) {
+        if (!!data[i - 1].date) {
+            break
+        }
+        ++j
+        --i
+    }
+
+    const casesNumber = parseInt(data[data.length - j]['Daily Cases'])
+    const casesNumberPrev = parseInt(data[data.length - (j+1)]['Daily Cases'])
 
     const casesColor = casesNumber > casesNumberPrev ? 'text-red-500' : 'text-green-500'
 
     return (
         <div>
             <h1 className={'text-2xl flex justify-center'}>
-                <span className={'text-black pr-5'}>{snap.nationalCases[snap.nationalCases.length - 1].date}</span>
+                <span className={'text-black pr-5'}>{data[data.length - 1].date}</span>
                 {casesNumber > casesNumberPrev ? <span className={casesColor}>&#8593; {casesNumber} Cases</span> : <span className={casesColor}>&#8595; {casesNumber} Cases</span>}
             </h1>
         </div>
